dca3a7c20d082c6b1dfdd64c91b7f27d03352f68
daniel13developer <daniel.developer13@hotmail.com>
2025-01-15 17:31:23 -0300

    Correção de erro do label da pagina de login

--- website-login/src/app/components/primary-input/primary-input.component.ts
+++ website-login/src/app/components/primary-input/primary-input.component.ts
@@ -1,6 +1,6 @@
 import { Text } from '@angular/compiler';
-import { Component, Input, input } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, forwardRef, Input, input, resolveForwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 
 type InputTypes = "text" | "email" | "password"
 
@@ -9,12 +9,46 @@ type InputTypes = "text" | "email" | "password"
   imports: [
     ReactiveFormsModule
   ],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => PrimaryInputComponent),
+      multi: true
+    }    
+  ],
   templateUrl: './primary-input.component.html',
   styleUrl: './primary-input.component.scss'
 })
-export class PrimaryInputComponent {
-  @Input() type: InputTypes = "text";
-  @Input() formName: string = "";
+export class PrimaryInputComponent implements ControlValueAccessor { 
+  @Input() type: InputTypes = "text";  
   @Input() placeholder: string ="";
   @Input() label: string = "";
+  @Input() inputName: string = "";
+
+  value: string = ''
+  OnChange: any = () => {}
+  OnTouched: any = () => {}
+
+  onInput(event: Event){
+    const value = (event.target as HTMLInputElement).value
+    this.OnChange(value) 
+  }
+
+  writeValue(value: any): void {
+    this.value = value;   
+  }
+
+  registerOnChange(fn: any): void {
+    this.OnChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.OnTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+   
+  }
+
 }
+
